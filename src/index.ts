67e35c71795fae85c98b519095afc1ff05d3c449
gudatta@microsoft.com <gudatta@microsoft.com>
2022-02-03 09:39:31 -0500

    Moved jslink code to its own file.

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,13 @@
-import { Helper } from "gd-sprest-bs";
+import { ContextInfo } from "gd-sprest-bs";
 import { Configuration } from "./cfg";
-import { Menu } from "./menu";
+import { register } from "./jslink";
 import "./styles.css";
 
-declare var RenderHeaderTemplate, RenderFooterTemplate;
-
-// Create the JSLink
-Helper.JSLink.register({
-    Templates: {
-        Header: ctx => {
-            // See if the TextDecoder global variable exists
-            if (window["TextDecoder"] == null) {
-                // Load the text-encoder
-                var s = document.createElement("script");
-                s.src = "https://unpkg.com/text-encoding@0.6.4/lib/encoding-indexes.js";
-                document.head.appendChild(s);
-                s = document.createElement("script");
-                s.src = "https://unpkg.com/text-encoding@0.6.4/lib/encoding.js";
-                document.head.appendChild(s);
-            }
-
-            // Render the default header
-            return "<div id='sc-admin-header'>" + RenderHeaderTemplate(ctx) + "</div>";
-        },
-        Footer: ctx => {
-            // See if the menu item doesn't exist
-            if (!Menu.Exists()) {
-                // Get the header element
-                let elViewControl = document.querySelector("#sc-admin-header .ms-csrlistview-controldiv");
-                if (elViewControl) {
-                    // Create the menu element
-                    let elMenu = document.createElement("div");
-                    elMenu.classList.add("ms-InlineSearch-DivBaseline");
-                    elMenu.style.paddingRight = "7px";
-
-                    // Get the inline search element
-                    let elSearch = elViewControl.querySelector(".ms-InlineSearch-DivBaseline");
-                    if (elSearch) {
-                        // Add the link after this
-                        elViewControl.insertBefore(elMenu, elSearch);
-                    } else {
-                        // Append the link
-                        elViewControl.appendChild(elMenu);
-                    }
-
-                    // Create the menu
-                    new Menu(elMenu, ctx);
-                }
-            }
-
-            // Render the default footer
-            return RenderFooterTemplate(ctx);
-        }
-    }
-});
-
 // Global Variable
-window["SCAdmin"] = { Configuration }
+window["SCAdmin"] = { Configuration, register }
+
+// See if this is a classic page and a list is detected
+if (ContextInfo.existsFl && ContextInfo.listId) {
+    // Register the jslink
+    register();
+}
---
+++ src/jslink.ts
@@ -0,0 +1,57 @@
+import { Helper } from "gd-sprest-bs";
+import { Menu } from "./menu";
+
+declare var RenderHeaderTemplate, RenderFooterTemplate;
+
+// Registers a JSLink w/ a list
+export function register() {
+    // Create the JSLink
+    Helper.JSLink.register({
+        Templates: {
+            Header: ctx => {
+                // See if the TextDecoder global variable exists
+                if (window["TextDecoder"] == null) {
+                    // Load the text-encoder
+                    var s = document.createElement("script");
+                    s.src = "https://unpkg.com/text-encoding@0.6.4/lib/encoding-indexes.js";
+                    document.head.appendChild(s);
+                    s = document.createElement("script");
+                    s.src = "https://unpkg.com/text-encoding@0.6.4/lib/encoding.js";
+                    document.head.appendChild(s);
+                }
+
+                // Render the default header
+                return "<div id='sc-admin-header'>" + RenderHeaderTemplate(ctx) + "</div>";
+            },
+            Footer: ctx => {
+                // See if the menu item doesn't exist
+                if (!Menu.Exists()) {
+                    // Get the header element
+                    let elViewControl = document.querySelector("#sc-admin-header .ms-csrlistview-controldiv");
+                    if (elViewControl) {
+                        // Create the menu element
+                        let elMenu = document.createElement("div");
+                        elMenu.classList.add("ms-InlineSearch-DivBaseline");
+                        elMenu.style.paddingRight = "7px";
+
+                        // Get the inline search element
+                        let elSearch = elViewControl.querySelector(".ms-InlineSearch-DivBaseline");
+                        if (elSearch) {
+                            // Add the link after this
+                            elViewControl.insertBefore(elMenu, elSearch);
+                        } else {
+                            // Append the link
+                            elViewControl.appendChild(elMenu);
+                        }
+
+                        // Create the menu
+                        new Menu(elMenu, ctx);
+                    }
+                }
+
+                // Render the default footer
+                return RenderFooterTemplate(ctx);
+            }
+        }
+    });
+}
