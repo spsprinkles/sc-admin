a9a8e20cc494fdf30a3bcd4db2b9ce7a15f86797
Dade Register <Dade.R@microsoft.com>
2023-07-26 14:29:36 -0600

    Update render method and globle variable

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,43 @@
+import { ContextInfo } from "gd-sprest-bs";
 import { Configuration } from "./cfg";
 import { register } from "./jslink";
 import { Dashboard } from "./dashboard";
 import { Menu } from "./menu";
+import Strings, { setContext } from "./strings";
 import "./styles.scss";
 
-// Global Variable
-window["SCAdmin"] = {
+// Create the global variable for this solution
+const GlobalVariable = {
     Configuration,
-    Dashboard,
+    Dashboard: null,
+    description: Strings.ProjectDescription,
     Menu,
     register,
-    render: (el, context) => { new Dashboard(el, context); }
-}
+    render: (el, context?, sourceUrl?: string) => {
+        // See if the page context exists
+        if (context) {
+            // Set the context
+            setContext(context, sourceUrl);
 
-// Register the JSLink
-register();
+            // Update the configuration
+            Configuration.setWebUrl(sourceUrl || ContextInfo.webServerRelativeUrl);
+        }
+
+        // Create the application
+        GlobalVariable.Dashboard = new Dashboard(el);
+    },
+    version: Strings.Version
+};
+
+// Make is available in the DOM
+window[Strings.GlobalVariable] = GlobalVariable;
+
+// Get the element and render the app if it is found
+let elApp = document.querySelector("#" + Strings.AppElementId) as HTMLElement;
+if (elApp) {
+    // Render the application
+    GlobalVariable.render(elApp);
+} else {
+    // Register the JSLink
+    register();
+}
---
+++ src/strings.ts
@@ -0,0 +1,25 @@
+import { ContextInfo } from "gd-sprest-bs";
+
+// Sets the context information
+// This is for SPFx or Teams solutions
+export const setContext = (context, sourceUrl?: string) => {
+    // Set the context
+    ContextInfo.setPageContext(context.pageContext);
+
+    // Update the source url
+    Strings.SourceUrl = sourceUrl || ContextInfo.webServerRelativeUrl;
+}
+
+/**
+ * Global Constants
+ */
+const Strings = {
+    AppElementId: "sc-admin",
+    DateFormat: "YYYY-MMM-DD",
+    GlobalVariable: "SCAdmin",
+    ProjectName: "Site Admin Tool",
+    ProjectDescription: "Tool to help manage SharePoint sites.",
+    SourceUrl: ContextInfo.webServerRelativeUrl,
+    Version: "0.0.6"
+};
+export default Strings;
