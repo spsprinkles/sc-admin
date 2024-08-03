a4f04f420ee5da94d65be3c38d979f32ac69a51c
Gunjan Datta <gudatta@microsoft.com>
2024-08-03 14:07:31 -0400

    Updated the version.

--- src/strings.ts
+++ src/strings.ts
@@ -26,6 +26,6 @@ const Strings = {
     SearchTerms: "phi pii ssn",
     SourceUrl: ContextInfo.webServerRelativeUrl,
     TimeFormat: "YYYY-MMM-DD HH:mm:ss",
-    Version: "0.1.6"
+    Version: "0.1.7"
 };
 export default Strings;
